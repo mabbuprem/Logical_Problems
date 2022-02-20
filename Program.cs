a357277fe460b608fbb7336a1a6ee64ead1d6264
mabbuprem <mabbupremkumar@gmail.com>
2022-02-20 18:17:46 +0530

    UC4_binarynumber

--- Program.cs
+++ Program.cs
@@ -10,24 +10,11 @@ namespace Logical
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("calculate monthly payment");
-            Console.WriteLine("enter principal amount");
-            double P = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("enter years");
-            double Y = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("enter percent interest");
-            double R = Convert.ToDouble(Console.ReadLine());
-            Calculate(P, Y, R);
-        }
-
-        public static void Calculate(double P, double Y, double R)
-        {
-            /*double r = R / (12 * 100);
-              double n = 12 * Y;
-              double payment = (P * r) / (1 - Math.Pow((1 + r), (-n)));*/
-            double totalAmount = P * (Math.Pow(1 + (R / 100), Y));
-            double EMI = totalAmount / (Y * 12);
-            Console.WriteLine(EMI);
+            Console.WriteLine("decimal to binary");
+            Console.WriteLine("enetr number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Binary binary = new Binary();
+            binary.GetBinary(number);
         }
     }
 }
