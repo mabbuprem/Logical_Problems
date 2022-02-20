83f654b92f478cd498012944d2f309693f5d7e3b
mabbuprem <mabbupremkumar@gmail.com>
2022-02-20 18:22:41 +0530

    perfect number

--- Program.cs
+++ Program.cs
@@ -10,11 +10,21 @@ namespace Logical
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("decimal to binary");
-            Console.WriteLine("enetr number: ");
+            Console.WriteLine("perfect number");
+            Console.WriteLine("enter number: ");
             int number = Convert.ToInt32(Console.ReadLine());
-            Binary binary = new Binary();
-            binary.GetBinary(number);
+            int maxLimit = (number / 2) + 1;
+            int sum = 0;
+            for (int i = 1; i < maxLimit; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            string result = number == sum ? "a" : "not a";
+            Console.WriteLine($"{number} is {result} perfect number");
+
         }
     }
-}
+ }
