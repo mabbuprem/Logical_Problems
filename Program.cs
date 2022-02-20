2ba071b8ea49f2b920c0911dc7efdf385224d8f5
mabbuprem <mabbupremkumar@gmail.com>
2022-02-20 17:58:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical
+{
+    internal class Programs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welocome to Logical Programs");
+            Console.Write("Enter last number of the series: ");
+            //VARIABLES
+            int lastNumber = Convert.ToInt32(Console.ReadLine());
+            int j = 1;
+            int i = 0;
+            int k = 0;
+
+            Console.Write(i + " " + j + " ");
+            while (k < lastNumber - i)
+            {
+                k = j + i;
+                Console.Write(k + " ");
+                i = j;
+                j = k;
+            }
+        }
+    }
+}
