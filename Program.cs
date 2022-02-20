66176053f85c74b9c3d0bd64f028368fb0359a3f
mabbuprem <mabbupremkumar@gmail.com>
2022-02-20 18:14:19 +0530

    monthly payment

--- Program.cs
+++ Program.cs
@@ -10,47 +10,24 @@ namespace Logical
     {
         static void Main(string[] args)
         {
-        
-                Console.WriteLine("day of week");
-                Console.WriteLine("enter day");
-                int d = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("enter month");
-                int m = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("enter year");
-                int y = Convert.ToInt32(Console.ReadLine());
-                GetWeek(d, m, y);
-            }
-            public static void GetWeek(int d, int m, int y)
-            {
-                int y1 = y - (14 - m) / 12;
-                int x = y1 + y1 / 4 - y1 / 100 + y1 / 400;
-                int m1 = m + 12 * ((14 - m) / 12) - 2;
-                int d1 = (d + x + 31 * m1 / 12) % 7;
+            Console.WriteLine("calculate monthly payment");
+            Console.WriteLine("enter principal amount");
+            double P = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter years");
+            double Y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter percent interest");
+            double R = Convert.ToDouble(Console.ReadLine());
+            Calculate(P, Y, R);
+        }
 
-                switch (d1)
-                {
-                    case 0:
-                        Console.WriteLine("sunday");
-                        break;
-                    case 1:
-                        Console.WriteLine("monday");
-                        break;
-                    case 2:
-                        Console.WriteLine("tuesday");
-                        break;
-                    case 3:
-                        Console.WriteLine("wednesday");
-                        break;
-                    case 4:
-                        Console.WriteLine("thursday");
-                        break;
-                    case 5:
-                        Console.WriteLine("friday");
-                        break;
-                    case 6:
-                        Console.WriteLine("saturday");
-                        break;
-                }
-            }
+        public static void Calculate(double P, double Y, double R)
+        {
+            /*double r = R / (12 * 100);
+              double n = 12 * Y;
+              double payment = (P * r) / (1 - Math.Pow((1 + r), (-n)));*/
+            double totalAmount = P * (Math.Pow(1 + (R / 100), Y));
+            double EMI = totalAmount / (Y * 12);
+            Console.WriteLine(EMI);
         }
     }
+}
