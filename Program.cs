2cbd94b361878d78eed217d7b33144a48bd38218
mabbuprem <mabbupremkumar@gmail.com>
2022-02-20 18:36:36 +0530

    stopwatch

--- Program.cs
+++ Program.cs
@@ -10,19 +10,19 @@ namespace Logical
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("calculate square root using newtons method");
-            Console.WriteLine("enter number");
-            double number = Convert.ToDouble(Console.ReadLine());
-            GetRoot(number);
-        }
-        public static void GetRoot(double number)
-        {
-            double answer = number;
-            while ((Math.Abs(answer - number / answer)) > (1e-15 * answer))
-            {
-                answer = (answer + (number / answer)) / 2;
-            }
-            Console.WriteLine(answer);
+            Console.WriteLine("stopwatch");
+            var stopwatch = new Stopwatch();
+
+            Console.WriteLine("press enter to start");
+            Console.ReadLine();
+            stopwatch.Start();
+
+            Console.WriteLine("press enter to stop");
+            Console.ReadLine();
+            stopwatch.Stop();
+
+            var elapsed_time = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine($"{elapsed_time / 1000} seconds");
         }
     }
 }
