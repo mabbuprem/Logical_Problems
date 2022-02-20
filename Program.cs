1a4c0146999eb4556c874c971f887acfa83381c7
mabbuprem <mabbupremkumar@gmail.com>
2022-02-20 18:10:01 +0530

    day of month

--- Program.cs
+++ Program.cs
@@ -10,44 +10,47 @@ namespace Logical
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("coupon numbers");
-            Console.WriteLine("enter number:");
-            int number = Convert.ToInt32(Console.ReadLine());
-            GetRandom(number);
-        }
-
-        public static void GetRandom(int number)
-        {
-            int[] Array = new int[number];
-            int count = 0;
-            for (int i = 0; i < number; i++)
-            {
-                bool same = false;
-                Random random = new Random();
-                int randomNum = random.Next(100, 999);
-
-                foreach (int num in Array)
-                {
-                    if (num == randomNum)
-                    {
-                        same = true;
-                        break;
-                    }
-
-                }
-                if (same == true && i > 0)
-                {
-                    i--;
-                }
-                else
-                {
-                    Array[i] = randomNum;
-                }
-                count++;
+        
+                Console.WriteLine("day of week");
+                Console.WriteLine("enter day");
+                int d = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter month");
+                int m = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter year");
+                int y = Convert.ToInt32(Console.ReadLine());
+                GetWeek(d, m, y);
             }
-            Console.WriteLine($"total number of iterations: {count}");
-            Console.WriteLine(String.Join(" ", Array));
-        }
+            public static void GetWeek(int d, int m, int y)
+            {
+                int y1 = y - (14 - m) / 12;
+                int x = y1 + y1 / 4 - y1 / 100 + y1 / 400;
+                int m1 = m + 12 * ((14 - m) / 12) - 2;
+                int d1 = (d + x + 31 * m1 / 12) % 7;
 
+                switch (d1)
+                {
+                    case 0:
+                        Console.WriteLine("sunday");
+                        break;
+                    case 1:
+                        Console.WriteLine("monday");
+                        break;
+                    case 2:
+                        Console.WriteLine("tuesday");
+                        break;
+                    case 3:
+                        Console.WriteLine("wednesday");
+                        break;
+                    case 4:
+                        Console.WriteLine("thursday");
+                        break;
+                    case 5:
+                        Console.WriteLine("friday");
+                        break;
+                    case 6:
+                        Console.WriteLine("saturday");
+                        break;
+                }
+            }
+        }
     }
-}
